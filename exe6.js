ab83affacd1afda3d15105f7fd479482237cb4c2
Marlinton Aldrighi <marlinton13@gmail.com>
2024-04-29 18:22:16 -0300

    exe6

--- exe6.js
+++ exe6.js
@@ -1,2 +1,8 @@
 // Crie uma função que receba um array de strings como argumento e
 // retorne um novo array com as strings convertidas para maiúsculas.
+
+let arr = ['marlinton', 'aldrighi', 'computadores', 'teclados'];
+
+upperArr = arr.map((elemento) => elemento.toUpperCase())
+
+console.log(upperArr);
